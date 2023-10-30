0e3e303b2d4976e3a4202c64f64eb48ae2603abb
Ernest K <ernio789@gmail.com>
2023-10-30 22:50:36 +0100

    create reservation page

--- client/src/pages/cars/Reservation.tsx
+++ client/src/pages/cars/Reservation.tsx
@@ -1,5 +1,6 @@
+import * as Form from "@radix-ui/react-form";
 import { ArrowLeftIcon } from "@radix-ui/react-icons";
-import { Button, Container } from "@radix-ui/themes";
+import { Button, Container, TextFieldInput } from "@radix-ui/themes";
 import { Link, useParams } from "react-router-dom";
 
 function Reservation() {
@@ -13,6 +14,95 @@ function Reservation() {
           Back to car
         </Link>
       </Button>
+      <div className="grid gap-5 md:grid-cols-3 py-4">
+        <Form.Root className="FormRoot">
+          <div className="flex flex-col gap-3">
+            <Form.Field className="FormField" name="firstName">
+              <div className="flex justify-between items-end mb-2">
+                <Form.Label className="FormLabel">First name</Form.Label>
+                <Form.Message
+                  className="FormMessage text-sm"
+                  match="valueMissing"
+                >
+                  Please enter your first name
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <TextFieldInput placeholder="first name" required />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="FormField" name="firstName">
+              <div className="flex justify-between items-end mb-2">
+                <Form.Label className="FormLabel">First name</Form.Label>
+                <Form.Message
+                  className="FormMessage text-sm"
+                  match="valueMissing"
+                >
+                  Please enter your first name
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <TextFieldInput placeholder="John" required />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="FormField" name="lastName">
+              <div className="flex justify-between items-end mb-2">
+                <Form.Label className="FormLabel">Last name</Form.Label>
+                <Form.Message
+                  className="FormMessage text-sm"
+                  match="valueMissing"
+                >
+                  Please enter your last name
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <TextFieldInput placeholder="Doe" required />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="FormField" name="email">
+              <div className="flex justify-between items-end mb-2">
+                <Form.Label className="FormLabel">Email</Form.Label>
+                <Form.Message
+                  className="FormMessage text-sm"
+                  match="valueMissing"
+                >
+                  Please enter your email
+                </Form.Message>
+                <Form.Message
+                  className="FormMessage text-sm"
+                  match="typeMismatch"
+                >
+                  Please provide a valid email
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <TextFieldInput
+                  placeholder="john@doe.com"
+                  type="email"
+                  required
+                />
+              </Form.Control>
+            </Form.Field>
+            <Form.Field className="FormField" name="phoneNumber">
+              <div className="flex justify-between items-end mb-2">
+                <Form.Label className="FormLabel">Phone number</Form.Label>
+                <Form.Message
+                  className="FormMessage text-sm"
+                  match="valueMissing"
+                >
+                  Please enter your phone number
+                </Form.Message>
+              </div>
+              <Form.Control asChild>
+                <TextFieldInput placeholder="123456789" required />
+              </Form.Control>
+            </Form.Field>
+            <Form.Submit asChild>
+              <Button>Reserve</Button>
+            </Form.Submit>
+          </div>
+        </Form.Root>
+      </div>
     </Container>
   );
 }
